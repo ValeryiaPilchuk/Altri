c421d0f8feff5a14b51dab274bda0228a486dbc2
Asterisku <ferpaul1974@gmail.com>
2022-03-24 22:47:13 -0400

    adding new xml and kt/java files related to chabot features to be developed and fixed

---
+++ app/src/main/java/com/example/altri/ChatbotActivity.kt
@@ -0,0 +1,137 @@
+package com.example.altri
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.content.Intent
+import android.net.Uri
+import com.example.altri.data.Message
+import android.util.Log
+import androidx.constraintlayout.motion.utils.Oscillator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.altri.utils.BotResponse
+import com.example.altri.utils.Constants.OPEN_GOOGLE
+import com.example.altri.utils.Constants.OPEN_SEARCH
+import com.example.altri.utils.Constants.RECEIVE_ID
+import com.example.altri.utils.Constants.SEND_ID
+import com.example.altri.utils.Time
+import kotlinx.android.synthetic.main.activity_chat_bot.*
+import kotlinx.coroutines.*
+import java.sql.Timestamp
+
+class ChatbotActivity: Activity() {
+
+    private lateinit var adapter: MessagingAdapter
+    private val botlist = listOf("Altri")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        //this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_chat_bot)
+        //val background: ConstraintLayout = findViewById(R.id.)
+
+        recyclerView()
+
+        clickEvents()
+
+        //val random = (0..0).random() //choose to always speak to altri
+        customMessage("Hello!, this is ${botlist[0]} speaking, how may I help you today? :)") //first msg in recyclerview
+
+    }
+
+    private fun clickEvents() {
+        btn_send.setOnClickListener(){
+            sendMessage()
+        }
+        et_message.setOnClickListener{
+            GlobalScope.launch {
+                delay(1000)
+                withContext(Dispatchers.Main){
+                    rv_messages.scrollToPosition(adapter.itemCount - 1) //keep us at the bottom of the screen
+                }
+            }
+        }
+    }
+
+
+    private fun recyclerView(){
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        rv_messages.layoutManager = LinearLayoutManager(applicationContext)
+    }
+
+    private fun sendMessage(){
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        if(message.isNotEmpty()){
+            et_message.setText("")
+            adapter.insertMessage(Message(message, SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+            botResponse(message)
+        }
+    }
+
+    private fun botResponse(message: String){
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch{
+            delay(1000) //1 second
+
+            withContext(Dispatchers.Main){
+                val response = BotResponse.basicResponses(message)
+
+                adapter.insertMessage(Message(response, RECEIVE_ID, timeStamp))
+                rv_messages.scrollToPosition(adapter.itemCount - 1) //take us down all the way to the last message in order to stay up to date
+
+
+                when(response){
+                    OPEN_GOOGLE ->{
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    OPEN_SEARCH ->{
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfter("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                rv_messages.scrollToPosition(adapter.itemCount - 1) //keep us at the bottom of the screen
+            }
+
+        }
+    }
+
+
+    private fun customMessage(message: String){
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                val timeStamp = Time.timeStamp()
+                adapter.insertMessage(Message(message, RECEIVE_ID, timeStamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount - 1) //take us down all the way to the last message in order to stay up to date
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/altri/MessagingAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.altri
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import com.example.altri.R
+import com.example.altri.data.Message
+
+import com.example.altri.utils.Constants.SEND_ID
+import com.example.altri.utils.Constants.RECEIVE_ID
+import kotlinx.android.synthetic.main.message_item.view.*
+import org.w3c.dom.Text
+
+class MessagingAdapter: RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
+    var messagesList = mutableListOf<Message>()
+    //private TextView tv_message
+
+
+    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        init {
+            itemView.setOnClickListener{
+                messagesList.removeAt(adapterPosition) //get message at point clicked on
+                notifyItemRemoved(adapterPosition) //complete removal with notification
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+       return MessageViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val currentMessage = messagesList[position]
+
+        when(currentMessage.id){
+            SEND_ID -> {
+                holder.itemView.tv_message.apply{
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.tv_bot_message.visibility = View.GONE
+            }
+            RECEIVE_ID -> {
+                holder.itemView.tv_bot_message.apply{
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.tv_message.visibility = View.GONE
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return messagesList.size
+    }
+    fun insertMessage(message: Message){
+        this.messagesList.add(message)
+        notifyItemInserted(messagesList.size)
+    }
+}
+
+
+
+
+
+
