dc6581928566ed5cee40cf866cb6e72f2e4fa603
Paul Fernandez <ferpaul1974@gmail.com>
2022-04-03 19:01:37 -0400

    adding bot speech prototype and button functionality

--- app/src/main/java/com/example/altri/ChatbotMenu.java
+++ app/src/main/java/com/example/altri/ChatbotMenu.java
@@ -24,14 +24,26 @@ public class ChatbotMenu extends AppCompatActivity {
         setContentView(R.layout.activity_chatbot_menu);
 
         Button chat = findViewById(R.id.text_to_altri);
+        Button voice = findViewById(R.id.speak_to_altri);
 
         Intent chatbot = new Intent(this, ChatbotActivity.class);
+        Intent chatvoice = new Intent(this, ChatbotVoice.class);
+
 
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "onClick screen");
                 startActivity(chatbot);
+                finish();
+            }
+        });
+
+        voice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick screen");
+                startActivity(chatvoice);
                 finish();
             }
         });
